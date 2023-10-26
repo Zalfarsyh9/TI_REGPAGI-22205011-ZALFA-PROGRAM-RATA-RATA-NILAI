c1c6c6fe22347aa3b7d269a05ff91c7bb29710e6
Zalfarsyh9 <145966652+Zalfarsyh9@users.noreply.github.com>
2023-10-26 12:36:47 +0700

    Add files via upload

---
+++ src/main/java/com/mycompany/ti_regpagi/latihan21/zalfa/java/TI_REGPAGI222050Latihan21ZalfaJava.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ti_regpagi.latihan21.zalfa.java;
+import java.util.Scanner;
+        
+/**
+ *
+ * @author User
+ */
+public class TI_REGPAGI222050Latihan21ZalfaJava {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Menerima input jumlah mahasiswa
+        System.out.print("Masukkan banyaknya mahasiswa: ");
+        int banyakMahasiswa = input.nextInt();
+
+        // Menerima input nilai mahasiswa
+        int[] nilaiMahasiswa = new int[banyakMahasiswa];
+        for (int i = 0; i < banyakMahasiswa; i++) {
+            System.out.print("Nilai mahasiswa ke-" + (i + 1) + ": ");
+            nilaiMahasiswa[i] = input.nextInt();
+        }
+
+        // Menghitung rata-rata nilai
+        int totalNilai = 0;
+        for (int nilai : nilaiMahasiswa) {
+            totalNilai += nilai;
+        }
+        float rataRataNilai = (float) totalNilai / banyakMahasiswa;
+
+        // Mencetak rata-rata nilai
+        System.out.println("Rata-rata nilainya adalah: " + rataRataNilai);
+    }
+}
